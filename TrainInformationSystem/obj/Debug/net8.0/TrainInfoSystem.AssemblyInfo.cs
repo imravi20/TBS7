95d3d958af7ec6880e498a81a5e869f041bc494c
imravi20 <rabimajumder08@gmail.com>
2025-05-12 02:48:38 +0530

    removedAView

--- TrainInformationSystem/obj/Debug/net8.0/TrainInfoSystem.AssemblyInfo.cs
+++ TrainInformationSystem/obj/Debug/net8.0/TrainInfoSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainInfoSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f351ecd0e86308ff54dbe2770fe4cd7e98c674f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainInfoSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainInfoSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
